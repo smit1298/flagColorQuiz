783334eb43e718b35ac6f0f2ad457e0bead5d0ad
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-02-26 17:09:54 +0100

    questioning
    question implemented

--- quizFlag.js
+++ quizFlag.js
@@ -8,22 +8,36 @@ flagGenerator.addEventListener("click", function (e) {
     .querySelector("img")
     .setAttribute("src", `https:${selectedCountryImage.file_url}`);
 
-  let countryName1 = document.getElementById("countryOne");
-  countryName1.textContent = selectedCountryImage.name;
+  // let countryName1 = document.getElementById("countryOne");
+  // countryName1.textContent = selectedCountryImage.name;
 
-  //   question
+
+  const allLabels = document.querySelectorAll("label")
+  allLabels[0].textContent = selectedCountryImage.name;
+  allLabels[1].textContent = countryWithImages[Math.floor(Math.random() * countryWithImages.length)].name
+  allLabels[2].textContent = countryWithImages[Math.floor(Math.random() * countryWithImages.length)].name
+  allLabels[3].textContent = countryWithImages[Math.floor(Math.random() * countryWithImages.length)].name
+  
+  
+  //   question for flag selection
   const question = document.querySelector("p");
-  question.textContent = `What is the name of the country depicted above ${"&quest;"}`;
+  question.textContent = `What is the name of the country depicted above ${"?"}`;
 
-  // generating second random option for country in the second radio
+  // // generating second random option for country in the second radio
 
-  let randomOption1 = countryWithImages[Math.floor(imagePicker * (2 - 1.6))];
+  // let randomOption1 = countryWithImages[Math.floor(Math.random() * countryWithImages.length)];
 
-  let countryName2 = document.getElementById("countryTwo");
-  countryName2.textContent = randomOption1.name;
+  // let countryName2 = document.getElementById("countryTwo");
+  // countryName2.textContent = randomOption1.name;
 
-  // generating third random option for country in the third radio
-  let randomOption2 = countryWithImages[Math.floor(imagePicker + 7)];
-  let countryName3 = document.getElementById("countryThree");
-  countryName3.textContent = randomOption2.name;
+  // // generating third random option for country in the third radio
+  // let randomOption2 = countryWithImages[Math.floor(Math.random() * countryWithImages.length)];
+  // let countryName3 = document.getElementById("countryThree");
+  // countryName3.textContent = randomOption2.name;
+
+  // // generating fourth random option for country in the fourth radio
+
+  //  let randomOption3 = countryWithImages[Math.floor(Math.random() * countryWithImages.length)];
+  //  let countryName4 = document.getElementById("countryFour");
+  //  countryName4.textContent = randomOption3.name;
 });
